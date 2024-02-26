3c321d75a6d7289ae222083d526a792fbb91563c
AaronGulman <81993286+AaronGulman@users.noreply.github.com>
2024-02-25 20:19:52 -0500

    Add JFrame, JPanel, and JLabel initialization in GUI constructor. Create JPanel with border in GUI constructor.

--- GUI.java
+++ GUI.java
@@ -1,7 +1,19 @@
+import javax.swing.BorderFactory;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 public class GUI{
 	private int clickCount = 0;
 	private JLabel clickLabel;
+
+	public GUI(){
+		JFrame frame = new JFrame();
+
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30,30,10,30));
+		
+		
+	}
 	
 }
